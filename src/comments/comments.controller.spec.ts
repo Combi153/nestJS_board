439a999093d348f29df395f72d12522c8cc4fe7e
Combi153 <ssrno009@gmail.com>
2025-02-09 19:03:05 +0900

    :sparkles: Post export 추가

--- src/comments/comments.controller.spec.ts
+++ src/comments/comments.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { TypeOrmTestingModule } from '../test-utils/type-orm-testing-module';
 
 describe('CommentsController', () => {
   let controller: CommentsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingModule()],
       controllers: [CommentsController],
       providers: [CommentsService],
     }).compile();
--- src/test-utils/type-orm-testing-module.ts
+++ src/test-utils/type-orm-testing-module.ts
@@ -1,8 +1,9 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from '../posts/domain/entities/post.entity';
-import { Comment } from '../posts/domain/entities/comment.entity';
+import { Comment } from '../comments/entities/comment.entity';
+import { DynamicModule } from '@nestjs/common';
 
-export const TypeOrmTestingModule = () => [
+export const TypeOrmTestingModule = (): DynamicModule[] => [
   TypeOrmModule.forRoot({
     type: 'better-sqlite3',
     database: ':memory:',
